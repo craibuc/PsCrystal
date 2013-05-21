8cd2410a244cf357ffd8c518576e8a396f7eea66
Craig Buchanan <craibuc@gmail.com>
2013-05-20 23:15:24 -0400

    Added Import-Lov function and examples.

---
+++ Examples/LoV/LoV.ps1
@@ -0,0 +1,6 @@
+﻿Import-Module PsCrystal -Force
+
+$lovPath = Convert-Path ".\LoV.txt"
+$rptPath = Convert-Path ".\Lov.rpt"
+
+Open-Report $rptPath | Import-Lov -path $lovPath -paramName "stringParam" -verbose | Save-Report -path ($rptPath -replace ".rpt", ("." + (Get-Date -f "yyyyMMdd-HHmmss") + ".rpt")) -verbose | Close-Report -verbose
