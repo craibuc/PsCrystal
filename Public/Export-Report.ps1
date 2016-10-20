e6b2ffcaf4070efb83a91974bc605d2cc2c25b6b
Craig Buchanan <craig.buchanan@cogniza.com>
2016-10-20 11:17:22 -0700

    Adding Export-Report function and tests

---
+++ Public/Export-Report.ps1
@@ -0,0 +1,46 @@
+﻿<#
+.SYNOPSIS
+    Refresh the report and export it to the specified format
+
+.DESCRIPTION
+.NOTES
+.LINK
+.EXAMPLE
+.INPUTTYPE
+.RETURNVALUE
+.COMPONENT
+.ROLE
+.FUNCTIONALITY
+.PARAMETER
+#>
+
+function Export-Report {
+
+	[cmdletbinding()]
+    param (
+        [Parameter(ValueFromPipeline=$true,Position=0,Mandatory=$true)][CrystalDecisions.CrystalReports.Engine.ReportDocument]$reportDocument,
+        [CrystalDecisions.Shared.ExportFormatType]$format=[CrystalDecisions.Shared.ExportFormatType]::PortableDocFormat,
+        [string]$path
+    )
+
+    begin {Write-Verbose "$($MyInvocation.MyCommand.Name)::Begin"}
+
+    process {
+
+        try {
+            Write-Verbose "Exporting $path ..."
+
+            #$reportDocument.ExportToDisk([CrystalDecisions.Shared.ExportFormatType]::PortableDocFormat, $path)
+            $reportDocument.ExportToDisk($format, $path)
+
+        }
+        catch [Exception] {
+            write-host $_.Exception
+        }
+        return $reportDocument
+
+    }
+
+    end {Write-Verbose "$($MyInvocation.MyCommand.Name)::End"}
+
+}
