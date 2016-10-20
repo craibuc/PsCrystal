dfc6ed938f7041029b8e13e2885a1258bab02b2f
Craig Buchanan <craig.buchanan@cogniza.com>
2016-10-20 11:18:22 -0700

    Adding Invoke-Report function and tests;

---
+++ Public/Invoke-Report.Tests.ps1
@@ -0,0 +1,9 @@
+﻿$here = Split-Path -Parent $MyInvocation.MyCommand.Path
+$sut = (Split-Path -Leaf $MyInvocation.MyCommand.Path) -replace '\.Tests\.', '.'
+. "$here\$sut"
+
+Describe "Invoke-Report" {
+    It "does something useful" {
+        $true | Should Be $false
+    }
+}
---
+++ Public/Invoke-Report.ps1
@@ -0,0 +1,41 @@
+﻿<#
+.SYNOPSIS
+    Refresh the report and its data.
+
+.DESCRIPTION
+.NOTES
+    This command may be unnecessary: Export-Report automatically generates report
+.LINK
+.EXAMPLE
+.INPUTTYPE
+.RETURNVALUE
+.COMPONENT
+.ROLE
+.FUNCTIONALITY
+.PARAMETER
+#>
+
+function Invoke-Report {
+
+	[cmdletbinding()]
+    param (
+        [Parameter(ValueFromPipeline=$true,Position=0,Mandatory=$true)][CrystalDecisions.CrystalReports.Engine.ReportDocument]$reportDocument
+    )
+
+	begin {Write-Verbose "$($MyInvocation.MyCommand.Name)::Begin"}
+
+    process {
+
+        try {
+            $reportDocument.Refresh
+        }
+        catch [Exception] {
+            write-host $_.Exception
+        }
+        return $reportDocument
+    }
+
+    end {Write-Verbose "$($MyInvocation.MyCommand.Name)::End"}
+
+
+}
