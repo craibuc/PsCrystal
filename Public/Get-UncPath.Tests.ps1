10c9326fe9f41ae89b2f1cc35a9204c053925c93
Craig Buchanan <craig.buchanan@cogniza.com>
2016-10-20 11:05:21 -0700

    Adding Get-UncPath function and tests

---
+++ Public/Get-UncPath.Tests.ps1
@@ -0,0 +1,45 @@
+﻿$here = Split-Path -Parent $MyInvocation.MyCommand.Path
+$sut = (Split-Path -Leaf $MyInvocation.MyCommand.Path) -replace '\.Tests\.', '.'
+. "$here\$sut"
+
+Describe "Get-UncPath" {
+
+	Context "Given a mapped drive" {
+
+		$folder = "N:\IT\Private\Clarity\EdgeReporting"
+		$expected = "\\enterprise.stanfordmed.org\depts\IT\Private\Clarity\EdgeReporting"
+
+	    It "Returns a UNC path" {
+	    	$actual = Get-UncPath -Path $folder -Verbose
+	        $actual | Should Be $expected
+	    }
+	}
+
+	Context "Given a file on mapped drive" {
+
+		$file = "N:\IT\Private\Clarity\EdgeReporting\BOE Migration - EXAMPLE.xlsx"
+		$expected = "\\enterprise.stanfordmed.org\depts\IT\Private\Clarity\EdgeReporting\BOE Migration - EXAMPLE.xlsx"
+
+	    It "Returns a UNC path" {
+	    	$actual = Get-UncPath -Path $file -Verbose
+	        $actual | Should Be $expected
+	    }
+	}
+
+	Context "Given items supplied via a pipeline" {
+
+		$items = @()
+		$items += "N:\IT\Private\Clarity\EdgeReporting"
+		$items += "N:\IT\Private\Clarity\EdgeReporting\BOE Migration - EXAMPLE.xlsx"
+
+		$expected = @()
+		$expected += "\\enterprise.stanfordmed.org\depts\IT\Private\Clarity\EdgeReporting"
+		$expected += "\\enterprise.stanfordmed.org\depts\IT\Private\Clarity\EdgeReporting\BOE Migration - EXAMPLE.xlsx"
+
+	    It "Returns a UNC path" {
+	    	$actual = $items | Get-UncPath -Verbose
+	        $actual | Should Be $expected
+	    }
+	}
+
+}
---
+++ Public/Get-UncPath.ps1
@@ -0,0 +1,53 @@
+﻿<#
+.SYNOPSIS
+    Convert a mapped drive (e.g. D:\) to its UNC representation (e.g. \\server\path\to\folder)
+.DESCRIPTION
+.NOTES
+Source:	http://superuser.com/a/29972/10768
+.LINK
+.EXAMPLE
+.INPUTTYPE
+.RETURNVALUE
+.COMPONENT
+.ROLE
+.FUNCTIONALITY
+.PARAMETER
+#>
+function Get-UncPath {
+
+    [cmdletbinding()]
+    param (
+        [Parameter(Position=0,Mandatory=$true,ValueFromPipelinebyPropertyName=$true,ValueFromPipeline=$true)]
+        [Alias('FullName')]
+        [string[]]$path
+    )
+    PROCESS {
+    	foreach ($p in $path) {
+    		Write-Verbose $p
+
+    		# get the FileInfo object specified by $path
+    		$item = Get-Item $p
+
+    		# folder or file
+    		switch ($item.PsIsContainer) {
+    			# folder
+    			$true	{
+    				$currentDrive = Split-Path -Qualifier $item.FullName
+    				Write-Debug $currentDrive
+    			}
+    			# file
+    			$false	{
+    				$currentDrive = Split-Path -Qualifier $item.DirectoryName
+    				Write-Debug $currentDrive
+    			}
+    		}
+
+			$logicalDisk = Gwmi Win32_LogicalDisk -filter "DriveType = 4 AND DeviceID = '$currentDrive'"
+			Write-Debug $logicalDisk.ProviderName
+
+			$uncPath = $path.Replace($currentDrive, $logicalDisk.ProviderName)
+			$uncPath
+
+    	} # /foreach
+    } # /PROCESS
+}
