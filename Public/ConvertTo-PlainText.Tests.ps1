2227430e4a3821fa4b32ca8d36be4d18daee6b24
Craig Buchanan <craig.buchanan@cogniza.com>
2019-05-16 10:57:25 -0500

    adding ConvertTo-PlainText

---
+++ Public/ConvertTo-PlainText.Tests.ps1
@@ -0,0 +1,14 @@
+﻿$here = Split-Path -Parent $MyInvocation.MyCommand.Path
+$sut = (Split-Path -Leaf $MyInvocation.MyCommand.Path) -replace '\.Tests\.', '.'
+. "$here\$sut"
+
+Describe "ConvertTo-PlainText" {
+
+	$expected = 'pa55w0rd'
+	$securePassword = ConvertTo-SecureString -String $expected -AsPlainText -Force
+
+    It "Convert a secure string to a plain-text string" {
+    	$actual = ConvertTo-PlainText $securePassword -Verbose
+        $actual | Should Be $expected
+    }
+}
---
+++ Public/ConvertTo-PlainText.ps1
@@ -0,0 +1,33 @@
+﻿<#
+.SUMMARY
+Convert an encrypted string ([System.Security.SecureString]) to its plain-text equivalent ([System.String])
+
+.DESCRIPTION
+Convert an encrypted string ([System.Security.SecureString]) to its plain-text equivalent ([System.String])
+
+.PARAMETER $SecureString
+The encrypted value to be converted.
+
+.EXAMPLE
+PS > $password = ConvertTo-SecureString 'pa55w0rd' -AsPlainText -Force
+PS > ConvertTo-PlainText $password
+'pa55word'
+
+.INPUTTYPE
+[System.Security.SecureString]
+
+.RETURNVALUE
+[System.String]
+#>
+function ConvertTo-PlainText {
+
+	[CmdletBinding()]
+	param(
+		[Parameter(Position = 0, Mandatory = $true)]
+		[SecureString]$SecureString
+	)
+
+	$BSTR = [System.Runtime.InteropServices.Marshal]::SecureStringToBSTR($SecureString)
+	[System.Runtime.InteropServices.Marshal]::PtrToStringAuto($BSTR)
+
+}
